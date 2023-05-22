d5c6e4187ad4b6daf60a318b9f589be0abd89314
NotBB <notice76610@gmail.com>
2023-05-22 14:06:33 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,48 @@
 class Client {
-      constructor(nom, prenom, dateNaissance, solde, numeroCompte) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.dateNaissance = dateNaissance;
-        this.solde = solde;
-        this.numeroCompte = numeroCompte;
-      }
+  constructor(nom, prenom, dateNaissance, solde, numeroCompte) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.dateNaissance = dateNaissance;
+    this.solde = solde;
+    this.numeroCompte = numeroCompte;
+  }
 
-      modifierSolde(montant) {
-        this.solde += montant;
-      }
+  modifierSolde(montant) {
+    this.solde += montant;
+  }
 
-      afficherInformations() {
-        const container = document.getElementById("clientsContainer");
+  afficherInformations() {
+    const table = document.getElementById("clientsTable");
 
-        const clientInfo = document.createElement("div");
-        clientInfo.innerHTML = `
-          <p>Prénom: ${this.prenom} <br> Numéro de compte: ${this.numeroCompte}</p>
-          <hr>
-        `;
+    const row = table.insertRow();
+    const nomCell = row.insertCell();
+    const numeroCompteCell = row.insertCell();
+    const actionCell = row.insertCell();
 
-        container.appendChild(clientInfo);
-      }
-    }
+    nomCell.textContent = this.nom;
+    numeroCompteCell.textContent = this.numeroCompte;
 
-    const clients = [];
+    const button = document.createElement("button");
+    button.textContent = "Modifier";
+    button.className = "button";
+    button.addEventListener("click", () => this.modifierSolde(100));
+    actionCell.appendChild(button);
+  }
+}
 
-    for (let i = 1; i <= 10; i++) {
-      const client = new Client(
-        "Nom" + i,
-        "Prénom" + i,
-        "1990-01-01",
-        1000 * i,
-        "Compte" + i
-      );
-      clients.push(client);
-    }
+const clients = [
+  new Client("Dupont", "Alice", "1990-05-15", 2500, "1234567890"),
+  new Client("Martin", "Jean", "1985-02-10", 1500, "0987654321"),
+  new Client("Lefebvre", "Sophie", "1992-08-20", 3500, "5678901234"),
+  new Client("Thomas", "Paul", "1978-11-28", 5000, "4321098765"),
+  new Client("Robert", "Emma", "1982-03-07", 1800, "7890123456"),
+  new Client("Gagnon", "Louis", "1995-06-02", 4200, "2109876543"),
+  new Client("Tremblay", "Julie", "1989-09-12", 3000, "6543210987"),
+  new Client("Lavoie", "Alexandre", "1991-12-25", 2800, "3456789012"),
+  new Client("Girard", "Marie", "1980-04-18", 2000, "8765432109"),
+  new Client("Beaulieu", "Luc", "1975-07-30", 4000, "9012345678")
+];
 
-    clients.forEach((client) => {
-      client.modifierSolde(500); // Ajoute 500 au solde de chaque client
-      client.afficherInformations(); // Affiche les informations de chaque client
-    });
+clients.forEach(client => {
+  client.afficherInformations(); // Affiche les informations
+});
